a4fdb1f2914c7e06c8dc0717e97494f4cc212994
Tobias Schulte <toschulte@gmail.com>
2025-06-10 07:27:38 +0200

    Handle missing console

--- Sample/Program.cs
+++ Sample/Program.cs
@@ -10,10 +10,28 @@ namespace Sample {
         }
 
         private static void SilenceConsole() {
+            // Don't try to read from the console when it is not attached. In
+            // framebuffer scenarios the console might be redirected which can
+            // cause exceptions or unnecessary CPU usage.
+            if (Console.IsInputRedirected) {
+                return;
+            }
+
             new Thread(() => {
-                Console.CursorVisible = false;
+                try {
+                    Console.CursorVisible = false;
+                } catch (Exception) {
+                    // Some environments don't allow changing the cursor state.
+                }
+
                 while (true) {
-                    _ = Console.ReadKey(true);
+                    try {
+                        _ = Console.ReadKey(true);
+                    } catch (InvalidOperationException) {
+                        // If there is no console available ReadKey will throw,
+                        // so pause briefly to avoid a tight loop.
+                        Thread.Sleep(100);
+                    }
                 }
             }) { IsBackground = true }.Start();
         }
